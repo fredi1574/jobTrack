2cc18bab9b895ce4ad093842af6ddcbb7a921502
Fredi1574 <Fredi1574@gmail.com>
2025-04-18 21:03:18 +0300

    Added tab icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.svg" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-gray-50 text-gray-900 antialiased dark:bg-gray-900 dark:text-gray-100`}
       >
