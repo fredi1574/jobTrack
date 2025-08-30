81ce5aa8063ca0a7f1aa441eaf7f41efd62a54e2
Fredi1574 <Fredi1574@gmail.com>
2025-08-30 21:58:51 +0300

    Redesigned the landing, login, and register pages

--- src/components/ClientWrapper.tsx
+++ src/components/ClientWrapper.tsx
@@ -1,26 +1,17 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import { usePathname } from "next/navigation";
 import { Toaster } from "sonner";
-import Header from "./Header";
-
-const NO_HEADER_PAGES: string[] = ["/login", "/register"];
+import HeaderWrapper from "./HeaderWrapper";
 
 interface ClientWrapperProps {
   children: React.ReactNode;
 }
 
 export default function ClientWrapper({ children }: ClientWrapperProps) {
-  // FIXME: fix the pathname header issue
-  const pathname = usePathname();
-  const shouldHideHeader = pathname
-    ? NO_HEADER_PAGES.includes(pathname)
-    : false;
-
   return (
     <SessionProvider>
       <Toaster theme="system" duration={2000} />
-      {!shouldHideHeader && <Header />}
+      <HeaderWrapper />
       {children}
     </SessionProvider>
   );
