19b2362faacfeeff0006ae0fc862f6627a818e61
Fredi1574 <Fredi1574@gmail.com>
2025-09-02 00:51:02 +0300

    Redundant

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function LandingPage() {
   return (
     <section className="relative flex min-h-screen items-center justify-center overflow-hidden bg-gradient-to-br from-blue-200 to-indigo-300 px-4 py-16 sm:px-6 lg:px-8 dark:from-gray-900 dark:to-gray-950">
-      {/* Background elements for wow effect */}
       <div className="absolute inset-0 z-0">
         <div className="absolute top-1/2 left-1/2 h-[1000px] w-[1000px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-to-r from-blue-300 to-rose-300 opacity-50 blur-3xl dark:from-blue-900 dark:to-purple-900"></div>
         <div className="absolute right-0 bottom-0 h-[600px] w-[600px] rounded-full bg-gradient-to-r from-teal-300 to-indigo-300 opacity-20 blur-3xl dark:from-teal-900 dark:to-indigo-900"></div>
