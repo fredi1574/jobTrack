b34fc08654079f71f1c4e3e24f51e4b8771a31cb
Fredi1574 <Fredi1574@gmail.com>
2025-09-09 21:40:18 +0300

    Removed the blur

--- src/components/ui/drawer.jsx
+++ src/components/ui/drawer.jsx
@@ -26,7 +26,8 @@ function DrawerOverlay({ className, ...props }) {
     <DrawerPrimitive.Overlay
       data-slot="drawer-overlay"
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50 backdrop-blur-xs",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
+
         className,
       )}
       {...props}
