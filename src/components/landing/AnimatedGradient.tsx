ff1081419f024a30f85296dc072636711be1387f
Fredi1574 <Fredi1574@gmail.com>
2025-09-21 21:44:14 +0300

    Added more gradients

--- src/components/landing/AnimatedGradient.tsx
+++ src/components/landing/AnimatedGradient.tsx
@@ -8,7 +8,7 @@ export default function AnimatedGradient() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 0.5, scale: 1 }}
         transition={{
-          duration: 1.5,
+          duration: 3.5,
           repeat: Infinity,
           repeatType: "reverse",
         }}
@@ -18,12 +18,34 @@ export default function AnimatedGradient() {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 0.2, scale: 1 }}
         transition={{
-          duration: 2,
+          duration: 5,
           delay: 0.5,
           repeat: Infinity,
           repeatType: "reverse",
         }}
         className="absolute right-0 bottom-0 h-[600px] w-[600px] rounded-full bg-gradient-to-r from-teal-300 to-indigo-300 opacity-20 blur-3xl dark:from-teal-900 dark:to-indigo-900"
+      />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 0.3, scale: 1 }}
+        transition={{
+          duration: 6,
+          delay: 1,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+        className="absolute top-0 left-0 h-[400px] w-[400px] rounded-full bg-gradient-to-r from-yellow-300 to-pink-300 opacity-30 blur-3xl dark:from-yellow-900 dark:to-pink-900"
+      />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 0.4, scale: 1 }}
+        transition={{
+          duration: 2.5,
+          delay: 1.5,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+        className="absolute right-1/2 bottom-0 h-[500px] w-[500px] rounded-full bg-gradient-to-r from-green-300 to-cyan-300 opacity-40 blur-3xl dark:from-green-900 dark:to-cyan-900"
       />
     </div>
   );
